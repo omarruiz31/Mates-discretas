05ea51242c364004c1676d55e2248a7fc3093434
Omar Ruiz <omarruiz@MacBook-Air-de-Omar.local>
2024-02-18 20:29:13 -0600

    Segunda fase

---
+++ Proyecto1.py
@@ -0,0 +1,14 @@
+#Factorizacion con el algoritmo de ecluides
+def euclides(num_1, num_2):
+    if num_2 == 0:
+        return num_1
+    else:
+        return euclides(num_2, num_1 % num_2)
+
+
+num_1 = int(input("Ingresa el primer número: "))
+num_2 = int(input("Ingresa el segundo número: "))
+
+mcd = euclides(num_1, num_2)
+
+print(f"{num_1} | {num_2}: MCD = {mcd}")
